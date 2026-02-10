9abeccb4413a31dbda83d53b02c717717e7b9b95
Mephistoooooooo <jkarldysulit@gmail.com>
2026-02-10 19:19:08 +0800

    FEAT:Layout Done

--- Sulit_GitHub_Calculator_Sample/Form1.cs
+++ Sulit_GitHub_Calculator_Sample/Form1.cs
@@ -16,5 +16,10 @@ namespace Sulit_GitHub_Calculator_Sample
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
