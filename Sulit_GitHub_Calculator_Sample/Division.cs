39012b61cf0d3a7529641a2fb6a653a44b3965af
Clark Ian Bautista <bautista.309855@balagtas.sti.edu.ph>
2026-02-10 19:59:36 +0800

    Created a Division Function for simple calculator.

---
+++ Sulit_GitHub_Calculator_Sample/Division.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sulit_GitHub_Calculator_Sample
+{
+    internal class Division
+    {
+        public double division(double num1, double num2)
+        {
+            return num1 / num2; // this method returns the divided value of num1 and num2
+        }
+    }
+}
